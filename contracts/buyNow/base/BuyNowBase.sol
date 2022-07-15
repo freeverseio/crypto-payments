9fb0b32dc653ceb752d56bc12e545cf2afc4a1f4
Toni Mateos <tmateos@freeverse.io>
2022-07-15 08:11:59 +0200

    Revert "relayedWithdraw does not fail if msg.sender == recipient"
    This reverts commit db4fdb5bb44a712177b630c74c5e274f573ece43.

--- contracts/buyNow/base/BuyNowBase.sol
+++ contracts/buyNow/base/BuyNowBase.sol
@@ -156,7 +156,7 @@ abstract contract BuyNowBase is IBuyNowBase, FeesCollectors, Operators {
     /// @inheritdoc IBuyNowBase
     function relayedWithdraw(address recipient) external {
         require(
-            !_onlyUserCanWithdraw[recipient] || (msg.sender == recipient),
+            !_onlyUserCanWithdraw[recipient],
             "BuyNowBase::relayedWithdraw: tx sender not authorized to withdraw on recipients behalf"
         );
         _withdrawAmount(recipient, _balanceOf[recipient], 0);
